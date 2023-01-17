15817a15fe66667061381510b1314c6ea0285e86
flaars <fcateloiu08@gmail.com>
2023-01-17 16:20:09 +0100

    Por ahora se spawnean los objetos invisibles.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     //Pieces
+    public GameObject[] piecesPrefab;
     public GameObject[] pieces;
     
     // Start is called before the first frame update
     void Start()
     {
-        SpawnNext();
+        CreateAllPieces();
     }
 
     // Update is called once per frame
@@ -22,9 +23,19 @@ public class Spawner : MonoBehaviour
     public void SpawnNext()
     {
         // Random Index
-        int i = Random.Range(0, this.pieces.Length);
+        int i = Random.Range(0, this.piecesPrefab.Length);
         
         // Spawn Group at current Position
-        Instantiate(this.pieces[i], this.transform.position, Quaternion.identity);
+        Instantiate(this.piecesPrefab[i], this.transform.position, Quaternion.identity);
+    }
+
+    public void CreateAllPieces()
+    {
+        pieces = new GameObject[7];
+        for (int i = 0; i < 7; i++)
+        {
+            this.pieces[i] = Instantiate(this.piecesPrefab[i], new Vector3(i*4, 30, 0), Quaternion.identity);
+            this.pieces[i].SetActive(false);
+        }
     }
 }
